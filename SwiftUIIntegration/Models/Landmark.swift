0233607a8315f063f98080c664d4adff785b2c62
Farhana Mustafa <farhana_mustafa@intuit.com>
2022-03-16 16:13:03 -0400

    add models

---
+++ SwiftUIIntegration/Models/Landmark.swift
@@ -0,0 +1,16 @@
+//
+//  Landmark.swift
+//  SwiftUIIntegration
+//
+//  Created by fmustafa on 3/16/22.
+//
+
+import Foundation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+}

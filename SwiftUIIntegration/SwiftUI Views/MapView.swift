bfdfa533becffd3748f6a3cdcbc0588fb192dd51
Farhana Mustafa <farhana_mustafa@intuit.com>
2022-03-16 21:44:18 -0400

    pass in coordinate as prop

--- SwiftUIIntegration/SwiftUI Views/MapView.swift
+++ SwiftUIIntegration/SwiftUI Views/MapView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+
     // You use the @State attribute to establish a source of truth for data in your app that you can modify from more than one view. SwiftUI manages the underlying storage and automatically updates views that depend on the value.
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
@@ -21,6 +23,7 @@ struct MapView: View {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        // pass in the coordinate property as a parameter
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
     }
 }

9cf5260cd2ac572ba99f8a96fdbaefa71cba5bb2
Farhana Mustafa <farhana_mustafa@intuit.com>
2022-03-16 21:52:13 -0400

    add private func

--- SwiftUIIntegration/SwiftUI Views/MapView.swift
+++ SwiftUIIntegration/SwiftUI Views/MapView.swift
@@ -12,12 +12,20 @@ struct MapView: View {
     var coordinate: CLLocationCoordinate2D
 
     // You use the @State attribute to establish a source of truth for data in your app that you can modify from more than one view. SwiftUI manages the underlying storage and automatically updates views that depend on the value.
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
+    @State private var region = MKCoordinateRegion()
+
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear { // view modifier that executes when appeared
+                setRegion(coordinate)
+            }
+    }
+
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 

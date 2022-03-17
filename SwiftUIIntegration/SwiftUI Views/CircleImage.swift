aa51e060cea7ae5e2e76093de2e7d38036bc36f9
Farhana Mustafa <farhana_mustafa@intuit.com>
2022-03-16 21:42:20 -0400

    refactor to using image as a stored property

--- SwiftUIIntegration/SwiftUI Views/CircleImage.swift
+++ SwiftUIIntegration/SwiftUI Views/CircleImage.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
     var body: some View {
-        Image("turtlerock")
+        image
             .resizable()
             .frame(width: 200.0, height: 200.0)
             .clipShape(Circle())
@@ -22,6 +23,6 @@ struct CircleImage: View {
 
 struct CircleImage_Previews: PreviewProvider {
     static var previews: some View {
-        CircleImage()
+        CircleImage(image: Image("turtlerock"))
     }
 }
